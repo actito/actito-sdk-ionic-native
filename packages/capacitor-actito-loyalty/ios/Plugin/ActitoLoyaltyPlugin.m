6aa629191224d41fdf89fe9883288c21baa81c41
yevheniismirnov <yevhenii.smirnov@actito.com>
2025-06-05 17:10:17 +0100

    feat(loyalty): add module

---
+++ packages/capacitor-actito-loyalty/ios/Plugin/ActitoLoyaltyPlugin.m
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+#import <Capacitor/Capacitor.h>
+
+// Define the plugin using the CAP_PLUGIN Macro, and
+// each method the plugin supports using the CAP_PLUGIN_METHOD macro.
+CAP_PLUGIN(ActitoLoyaltyPlugin, "ActitoLoyaltyPlugin",
+           CAP_PLUGIN_METHOD(fetchPassBySerial, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(fetchPassByBarcode, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(present, CAPPluginReturnPromise);
+)
