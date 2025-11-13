d2f91edd54981fb5db3eb5f3ced10d580f704857
yevheniismirnov <yevhenii.smirnov@actito.com>
2025-11-13 10:01:49 +0100

    feat: update actito native libs

--- packages/capacitor-actito-in-app-messaging/Package.swift
+++ packages/capacitor-actito-in-app-messaging/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
-        .package(url: "git@github.com:actito/actito-sdk-ios-in-house-releases.git", from: "5.0.0-canary.5"),
+        .package(url: "https://github.com/Actito/actito-sdk-ios.git", from: "5.0.0-beta.1"),
     ],
     targets: [
         .target(
@@ -19,8 +19,8 @@ let package = Package(
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
-                .product(name: "ActitoKit", package: "actito-sdk-ios-in-house-releases"),
-                .product(name: "ActitoInAppMessagingKit", package: "actito-sdk-ios-in-house-releases"),
+                .product(name: "ActitoKit", package: "actito-sdk-ios"),
+                .product(name: "ActitoInAppMessagingKit", package: "actito-sdk-ios"),
             ],
             path: "ios/Sources/ActitoInAppMessagingPlugin"),
     ]
--- packages/capacitor-actito/CapacitorActito.podspec
+++ packages/capacitor-actito/CapacitorActito.podspec
@@ -1,7 +1,7 @@
 require 'json'
 
 package = JSON.parse(File.read(File.join(__dir__, 'package.json')))
-actito_version = '5.0.0-canary.5'
+actito_version = '5.0.0-beta.1'
 
 Pod::Spec.new do |s|
   s.name = 'CapacitorActito'
